f768e12cc0ff5dd51f9b601cf6ef41eba9ae9c96
AL-SULTAN <alsultan.it@outlook.com>
2022-07-19 06:30:33 +0300

    AppDelegate window implemetation

--- TransactionNative.iOS/Application/AppDelegate.cs
+++ TransactionNative.iOS/Application/AppDelegate.cs
@@ -6,6 +6,8 @@ namespace TransactionNative.iOS
     [Register("AppDelegate")]
     public class AppDelegate : UIApplicationDelegate
     {
+        public override UIWindow Window { get; set; }
+
         public override bool FinishedLaunching(UIApplication Application, NSDictionary LaunchOptions) => true;     
     }
 }
